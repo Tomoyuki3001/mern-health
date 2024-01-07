a74525d13172b3d05369b9c5427ce8f4407b1a48
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-06 22:22:50 -0800

    Created the basic login page design

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import { Button } from "antd";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App p-5">
-      <h1>Hello Health!</h1>
-      <Button type="primary">Primary Button</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const onFinish = (values) => {
+    console.log("Received values of the form:", values);
+  };
+  return (
+    <div className="auth">
+      <div className="auth-form card p-4">
+        <h1 className="card-title">Nice to meet you</h1>
+        <Form layout="vertical" onFinish={onFinish}>
+          <Form.Item label="Name" name="name">
+            <Input placeholder="Name" />
+          </Form.Item>
+          <Form.Item label="Email" name="email">
+            <Input placeholder="Email" />
+          </Form.Item>
+          <Form.Item label="Password" name="password">
+            <Input placeholder="Password" type="password" />
+          </Form.Item>
+          <Button className="primary-button my-4" htmlType="submit">
+            REGISTER
+          </Button>
+          <Link to="/login" className="auth-link">
+            Click to the Login page
+          </Link>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
