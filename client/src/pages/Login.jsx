b96394b375a5a2358add6ba28a4b67aa589e607d
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-07 16:41:46 -0800

    Connected with the MongoDB

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,11 +9,8 @@ const Login = () => {
   return (
     <div className="auth">
       <div className="auth-form card p-4">
-        <h1 className="card-title">Nice to meet you</h1>
+        <h1 className="card-title">Welcome Back</h1>
         <Form layout="vertical" onFinish={onFinish}>
-          <Form.Item label="Name" name="name">
-            <Input placeholder="Name" />
-          </Form.Item>
           <Form.Item label="Email" name="email">
             <Input placeholder="Email" />
           </Form.Item>
@@ -23,8 +20,8 @@ const Login = () => {
           <Button className="primary-button my-4" htmlType="submit">
             REGISTER
           </Button>
-          <Link to="/login" className="auth-link">
-            Click to the Login page
+          <Link to="/register" className="auth-link">
+            Click to the Signup page
           </Link>
         </Form>
       </div>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const Register = () => {
-  const onFinish = (values) => {
-    console.log("Received values of the form:", values);
+  const onFinish = async (values) => {
+    try {
+      const response = await axios.post("/api/user/register", values);
+      if (response.data.success) {
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      toast.error("Something wrong");
+    }
   };
   return (
     <div className="auth">
