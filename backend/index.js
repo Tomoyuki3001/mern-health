2ff47c1aa037fbe3da06fc093353433da8555780
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-08 14:37:30 -0800

    Updated the env file

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 const dbConfig = require("./config/dbConfig");
+dbConfig;
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 
