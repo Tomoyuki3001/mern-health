36f1641f5f04056187193bb6a144b54970ddab8b
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-08 10:50:44 -0800

    Updated the back-end files

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 
 const dbConfig = require("./config/dbConfig");
 app.use(express.json());
