05bcda9eaa7e6b55577dd6bb4092c48dc99e060a
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-09 12:15:51 -0800

    Added the CORS line

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 
-// app.use(
-//   cors({
-//     origin: ["https://mern-health-front-end.vercel.app"],
-//     methods: ["POST", "GET"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["https://mern-health-front-end.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 const mongoose = require("mongoose");
 
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
 app.use(express.json());
 const userRoute = require("./routes/userRoute");
 
-app.use("https://mern-health.vercel.app", userRoute);
+app.use("/api", userRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Node server started at port ${port}`));
