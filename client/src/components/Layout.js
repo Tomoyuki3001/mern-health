9745bd8bb3d453141cea9ed13d194d9d4b1147da
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-01-11 16:19:34 -0800

    Created the user route and user profile link

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/layout.css";
 import { Link, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Layout = ({ children }) => {
+  const [collapsed, setCollapsed] = useState(false);
+  const { user } = useSelector((state) => state.user);
   const location = useLocation();
   const userMenu = [
     { name: "Home", path: "/", icon: "ri-home-line" },
@@ -19,7 +22,7 @@ const Layout = ({ children }) => {
   return (
     <div className="main">
       <div className="d-flex layout">
-        <div className="slidebar">
+        <div className="sidebar">
           <div className="sidebar-header">
             <h1>TM</h1>
           </div>
@@ -33,14 +36,32 @@ const Layout = ({ children }) => {
                   }`}
                 >
                   <i className={menu.icon}></i>
-                  <Link to={menu.path}>{menu.name}</Link>
+                  {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
                 </div>
               );
             })}
           </div>
         </div>
         <div className="content">
-          <div className="header">Header</div>
+          <div className="header">
+            {collapsed ? (
+              <ieade
+                className="ri-menu-2-line header-action-icon"
+                onClick={() => setCollapsed(false)}
+              ></ieade>
+            ) : (
+              <i
+                className="ri-close-line header-action-icon"
+                onClick={() => setCollapsed(true)}
+              ></i>
+            )}
+            <div className="d-flex align-items-center px-4">
+              <i className="ri-notification-2-line header-action-icon mr-2 px-3"></i>
+              <Link className="auth-link" to="/profile">
+                {user?.name}
+              </Link>
+            </div>
+          </div>
           <div className="body">{children}</div>
         </div>
       </div>
